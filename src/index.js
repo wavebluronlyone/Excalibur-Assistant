daf21d76e0205380381873b99f8fe0361efc0339
CuteCoder <wave_onlyone@hotmail.com>
2018-11-19 23:03:48 +0700

    Fix bug on index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const fastify = require('fastify')();
 const cors = require('cors');
 const facebook = require('./routes/facebook');
-const line = require('./routes/facebook');
+const line = require('./routes/line');
 const port = process.env.PORT || 3002;
 
 fastify.use(cors());
-fastify.register(line, { prefix: '/webhooks/line' })
+fastify
+  .register(line, { prefix: '/webhooks/line' })
   .register(facebook, { prefix: '/webhooks/facebook' });
 
 fastify.get('/healthcheck', (req, reply) => {
